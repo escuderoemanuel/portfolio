8766a53ffd9b91b57832eb9fa4b165f86c0a5c0a
Emanuel Escudero <escudero.mdz@gmail.com>
2023-05-19 14:43:12 -0300

    Punto Control Portfolio Responsive

--- src/components/Body/Porfolio/Portfolio.jsx
+++ src/components/Body/Porfolio/Portfolio.jsx
@@ -6,12 +6,53 @@ const Portfolio = () => {
     <section className='portfolio' id='portfolio'>
       <h3 className='titlePortfolio'>Portfolio</h3>
       <div className='portfolio-content'>
+        {/* CARD */}
         <div className='cardPortfolio'>
           <img src='https://i.ibb.co/3S3366B/retroplace.jpg' alt='' />
           <div className='cardPortfolioDescription'>
             <h3>Title</h3>
             <h5>Type</h5>
-            <p>TOOL | TOOL | TOOL | TOOL | TOOL | TOOL JS</p>
+            <p>TOOL | TOOL | TOOL | TOOL | GIT | REACT JS</p>
+          </div>
+          <div className='btn-content'>
+            <button className='button-card-portfolio'>
+              <i class='bx bxl-github bx-sm'></i>
+              <p>GitHub</p>
+            </button>
+            <button className='button-card-portfolio'>
+              <i class='bx bx-link bx-sm'></i>
+              <p>Deploy</p>
+            </button>
+          </div>
+        </div>
+
+        {/* CARD */}
+        <div className='cardPortfolio'>
+          <img src='https://i.ibb.co/3S3366B/retroplace.jpg' alt='' />
+          <div className='cardPortfolioDescription'>
+            <h3>Title</h3>
+            <h5>Type</h5>
+            <p>TOOL | TOOL | TOOL | TOOL | GIT | REACT JS</p>
+          </div>
+          <div className='btn-content'>
+            <button className='button-card-portfolio'>
+              <i class='bx bxl-github bx-sm'></i>
+              <p>GitHub</p>
+            </button>
+            <button className='button-card-portfolio'>
+              <i class='bx bx-link bx-sm'></i>
+              <p>Deploy</p>
+            </button>
+          </div>
+        </div>
+
+        {/* CARD */}
+        <div className='cardPortfolio'>
+          <img src='https://i.ibb.co/3S3366B/retroplace.jpg' alt='' />
+          <div className='cardPortfolioDescription'>
+            <h3>Title</h3>
+            <h5>Type</h5>
+            <p>TOOL | TOOL | TOOL | TOOL | GIT | REACT JS</p>
           </div>
           <div className='btn-content'>
             <button className='button-card-portfolio'>
