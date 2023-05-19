b9d215f2977fd241752e44fd9eb8b690a903010b
Emanuel Escudero <escudero.mdz@gmail.com>
2023-05-19 11:14:52 -0300

    Punto Control Portfolio Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer/Footer';
 import LuzBg from './components/Global/LuzBg/LuzBg';
 import Header from './components/Header/Header';
 import Home from './components/Body/Home/Home';
+import Portfolio from './components/Body/Porfolio/Portfolio';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <LuzBg className={'Luz3'}></LuzBg>
       <LuzBg className={'Luz4'}></LuzBg>
       <Home></Home>
+      <Portfolio></Portfolio>
       <Footer></Footer>
     </>
   );
