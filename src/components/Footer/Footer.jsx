afadd9767472e051881460e276a79e7b130870a6
Emanuel Escudero <escudero.mdz@gmail.com>
2023-06-26 22:52:05 -0300

    Cambios en footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './Footer.css';
+import { useTranslation } from 'react-i18next';
+
 const Footer = () => {
+  /* i18Next Translation */
+  const [t, i18n] = useTranslation('global');
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
   return (
     <section className='footer'>
       <div className='footerContainer'>
-        <p>© 2023 | All Rights Reserved</p>
-        <p>[ Powered by Emanuel Escudero ]</p>
+        <p>{t('footer.paragraph')}</p>
+        <img
+          className='logo'
+          src={t('footer.img.url')}
+          alt={t('footer.img.alt')}
+          onClick={handleScrollToTop}
+        />
         <span className='luzFooter'></span>
       </div>
     </section>
